1ca44a02ed33d6d3a3f66af74539ec689e254edc
KIMJONGHAK <86517369+JHlab123@users.noreply.github.com>
2025-05-30 03:36:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,17 +10,22 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name + '/config', ['config/params.yaml']),
-        ('share/' + package_name + '/launch', ['launch/dwa_planner_launch.py']),
+        ('share/' + package_name + '/launch', [
+            'launch/dwa_planner_launch.py',
+            'launch/test_evasion_launch.py'
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='JONGHAKKIM',
-    maintainer_email='dwefresh1@inha.edu',
-    description='DWA Planner in Python for ROS 2',
-    license='Apache License 2.0',
+    maintainer='Your Name',
+    maintainer_email='you@your.org',
+    description='Full-port of AMSL DWA Planner to Python/ROS2',
+    license='Apache-2.0',
     entry_points={
         'console_scripts': [
             'dwa_planner_node = dwa_planner_ros2.dwa_planner_node:main',
+            'fake_sensor_publisher = dwa_planner_ros2.fake_sensor_publisher:main',
+            'fake_goal_publisher = dwa_planner_ros2.fake_goal_publisher:main',
         ],
     },
 )
